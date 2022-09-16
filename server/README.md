b3152faf0934caf4f7495fa4e2fcff6cc04cb788
ppconrado <ppconrado@yahoo.com.br>
2022-09-16 08:01:15 -0300

    use cases routes

---
+++ server/README.md
@@ -0,0 +1,43 @@
+# Back-end
+
+### Entities
+
+### Game
+
+id:
+title:
+bannerUrl:
+
+CDN (Content Delivery Network) - Amazon S3 - noUrl
+
+### Ad
+
+id:
+gameId:
+name:
+yearsPlaying:
+discord:
+weekDays:
+horsStart:
+hoursEnd:
+useVoiceChannel:
+createdAt:
+
+TIP:
+
+1:30h => 90 min
+R$ 179,89 +> 17989
+
+Datas (fuso horario / formatos diferentes)
+Pontos flutuantes
+
+## Casos de uso
+
+### Como o usuario vai intergir, quais acoes podem ser utilizadas
+
+### -> rotas -> comunicacao com o backend vai ser um caso de uso
+
+1 - listagem de games com contagem de anuncios
+2 - Criacao de anuncio
+3 - Listagem de anuncios por game
+4 - Buscar discord pelo ID do anuncio
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,41 @@
-import express from "express";
+import express, { request, response } from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
+// HTTP methods / API RESTful / HTTP Codes .status(200)
+
+/**
+ * Query: ... localhost:3333/ads?page=2&sort=title
+ * Route: ... localhost:3333/ads/5
+ *            localhost:3333/post/como-criar-uma-api-node
+ * Body: ...  envio de formulario, criacao de usuario, informacao sensiveis
+ */
+
+// 1 - listagem de games com contagem de anuncios
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+// 2 - Criacao de anuncio
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+// 3 - Listagem de anuncios por game
+app.get("/games/:id/ads", (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
     { id: 1, name: "Anuncio 1" },
     { id: 2, name: "Anuncio 2" },
   ]);
+});
+
+// 4 - Buscar discord pelo ID do anuncio
+app.get("/ads/:id/discord", (request, response) => {
+  // const adId = request.params.id;
+
+  return response.json([]);
 });
 
 app.listen(3333);
