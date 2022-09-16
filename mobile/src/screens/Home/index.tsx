aff0d91febddf5124335344e7d3545f0c40104ac
ppconrado <ppconrado@yahoo.com.br>
2022-09-16 04:08:03 -0300

    Heading component

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image } from "react-native";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
 
 import { styles } from "./styles";
 
 export function Home() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+    </View>
+  );
 }
